575c82ec108f72f40edca48d15263c6bde36274d
hsmith <hsmith@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2006-10-14 01:25:35 +0000

    This commit was manufactured by cvs2svn to create tag 'start'.
    git-svn-id: http://svn.silverstripe.com/modules/regress@21791 467b73ca-7a2a-4603-9d3b-597d59a354a9

---
+++ tags/start/_config.php
@@ -0,0 +1,31 @@
+<?
+
+if($_SERVER[HTTP_HOST] == "test") {
+	header("Location: http://test.totallydigital.co.nz$_SERVER[REQUEST_URI]");
+	die();
+}
+if($_SERVER[HTTP_HOST] == "dev") {
+	header("Location: http://dev.totallydigital.co.nz$_SERVER[REQUEST_URI]");
+	die();
+}
+
+global $project;
+$project = "regress";
+
+$databaseConfig = array(
+	"type" => "MySQLDatabase",
+	"server" => "localhost", 
+	"username" => "silverstripe", 
+	"password" => "silverNli24yeg", 
+	"database" => "SS_regress",
+);
+
+
+Director::addRules(100, array(
+));
+
+Debug::sendLiveErrorsTo("support@silverstripe.com");
+
+Security::setDefaultAdmin("td", "2Bornot2B");
+
+?>
