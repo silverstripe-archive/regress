a72055dae0d1c3dc22d129c4575730849c2e5017
rspittel <rspittel@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2010-01-22 00:38:58 +0000

    ENHANCEMENT: - added additional status field for the session - added references to the ORM - add support for test-session to work with partial tests (tests on sections, not on test-plans).
    git-svn-id: http://svn.silverstripe.com/modules/regress@97420 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- trunk/code/TestSessionObj.php
+++ trunk/code/TestSessionObj.php
@@ -1,21 +1,36 @@
 <?php
+/**
+ * @package regress
+ * @subpackage code
+ */
 
+/**
+ *
+ */
 class TestSessionObj extends DataObject {
+	
 	static $db = array(
-		'Tester' => 'Varchar(20)',
-		'OverallNote' => 'Text'
+		'Tester'      => 'Varchar(20)',
+		'OverallNote' => 'Text',
+		"Status"      => "Enum(array('new','draft','submitted','archived'),'new')",
 	);
+	
 	static $has_one = array(
-		"TestPlan" => "TestPlan",
-		'Author' => 'Member', 
+		"TestPlan"    => "TestPlan",
+		"TestSection" => "TestSection",
+		'Author'      => 'Member', 
 	);
+	
 	static $has_many = array(
-		"Results" => "StepResult",
+		"Results"     => "StepResult",
 	);
+
+	static $defaults = array(
+	    'Status' => 'new',
+	 );
 	
 	function onBeforeWrite() {
 	   $this->AuthorID = Member::currentUserID();
-	   
 	   parent::onBeforeWrite();
 	}
 	
@@ -31,19 +46,60 @@ class TestSessionObj extends DataObject {
 		return DataObject::get("StepResult", "TestSessionID = $this->ID AND (Outcome = 'fail' OR (Outcome IN ('pass','skip') AND Note != '' AND Note IS NOT NULL))");
 	}
 	
+	/**
+	 * Return number (as string) of passed scenarios for the feature.
+	 *
+	 * @return String value of the first item of the {@link SS_Query} object
+	 */
 	function getNumPasses() {
 		return DB::query("SELECT COUNT(*) FROM StepResult WHERE TestSessionID = $this->ID AND Outcome = 'pass'")->value();
 	}
+
+	/**
+	 * Return number (as string) of failed scenarios for the feature.
+	 *
+	 * @return String value of the first item of the {@link SS_Query} object
+	 */
 	function getNumFailures() {
 		return DB::query("SELECT COUNT(*) FROM StepResult WHERE TestSessionID = $this->ID AND Outcome = 'fail'")->value();
 	}
 	
+	/**
+	 * Return number (as string) of skipped scenarios for the feature.
+	 *
+	 * @return String value of the first item of the {@link SS_Query} object
+	 */
 	function getNumSkips() {
 		return DB::query("SELECT COUNT(*) FROM StepResult WHERE TestSessionID = $this->ID AND Outcome = 'skip'")->value();
 	}
 	
+	function isEditable() {
+		return ($this->Status == 'new' || $this->Status == 'draft');
+	}
+	
+	/**
+	 * Returns the parent ID for this session. It can be a test-section or a 
+	 * test-plan ID.
+	 */
+	function getTestReferenceID() {
+		$id = '';
+		if ($this->TestPlanID) {
+			$id = $this->TestPlanID;
+		} else
+		if ($this->TestSectionID) {
+			$id = $this->TestSectionID;
+		} 
+		return $id;
+	}
+	
+	/**
+	 * Return link to the report for this feature.
+	 *
+	 * @return String relative link to the test-report.
+	 */
 	function Link() {
-		return 'testplan/reportdetail/' . $this->TestPlanID . '/' . $this->ID;
+		$id = $this->getTestReferenceID();
+		return 'session/reportdetail/' . $id . '/' . $this->ID;
 	}
 }
 
