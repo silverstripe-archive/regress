03e2cc525280847b2da7b5035d8ed27fa8a75a84
rspittel <rspittel@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2010-01-22 00:37:24 +0000

    ENHANCEMENT: - added markdown support - re-factored getCMSfields
    git-svn-id: http://svn.silverstripe.com/modules/regress@97419 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- trunk/code/Page.php
+++ trunk/code/Page.php
@@ -1,11 +1,33 @@
-<?
+<?php
+/**
+ * @package regress
+ * @subpackage code
+ */
 
+/**
+ * General page class for all regress site-tree classes. 
+ */
 class Page extends SiteTree {
+	
 	static $default_child = "TestPlan";
 	
 	static $db = array(
 	);
 	
+	static $casting = array(
+	);	
+	
+	
+	function Content() {
+		return Markdown($this->Content);
+	}
+	
+	/**
+	 * Overwrite SiteTree::getCMSFields method and remove most standard page
+	 * type fields.
+	 *
+	 * @return FieldSet
+	 */
 	function getCMSFields() {
 		$fields = parent::getCMSFields();
 		$fields->removeFieldFromTab("Root", "Content");
@@ -17,24 +39,54 @@ class Page extends SiteTree {
 		
 		$fields->addFieldToTab("Root.Edit", new TextField("Title", "Name"));
 		$fields->addFieldToTab("Root.Edit", new TextareaField("Content", "Description"));
+
+		// change order of the tabs so that edit is at the beginning again.
+		$resultsTab = $fields->fieldByName('Root.Results');
+		
+		if ($resultsTab != null) {
+			$editTab    = $fields->fieldByName('Root.Edit');
+			$fields->removeFieldFromTab("Root", "Edit");
+			$fields->addFieldToTab('Root',$editTab,"Results");
+		}
+
 		return $fields;
 	}
 	
+	/**
+	 * Extend CMS action handling: the only action allowed is to save the page.
+	 *
+	 * @return FieldSet
+	 */
 	function getAllCMSActions() {
 		return new FieldSet(
 			new FormAction("save", "Save changes")
 		);
 	}
 	
+	/**
+	 * Overwrite canCreate method. Just instances of TestPlan, TestSection and 
+	 * Page can be created.
+	 * 
+	 * @return boolean
+	 */
 	function canCreate() {
 		return $this->class == "TestPlan" || $this->class == "TestSection" || $this->class == "Page";
 	}
 	
+	/**
+	 * Overwrite TreeTitle method.
+	 *
+	 * @return boolean
+	 */
 	function TreeTitle() {
 		return $this->Title;
 	}
+
 }
 
+/**
+ * Controller class for the page-class.
+ */ 
 class Page_Controller extends ContentController {
 	function Menu1() {
 		return $this->getMenu(1);
