1f4bfa09c2b735c9b7aeb5019d9df15b7131fc02
rspittel <rspittel@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2010-01-22 00:33:11 +0000

    ENHANCEMENT: add save-session (draft session) behaviour.
    git-svn-id: http://svn.silverstripe.com/modules/regress@97416 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- trunk/javascript/TestPlan.js
+++ trunk/javascript/TestPlan.js
@@ -1,4 +1,44 @@
+
+(function($) {
+$(document).ready(function() {
+	
+	$('input[name=action_doSaveSession]').livequery('click', function(){
+		var form = $('form');
+		var formAction = form.attr('action') + '?' + 'action_doSaveSession=Execute'
+
+		$.post(formAction, form.formToArray(), function(result){
+			var response = eval('(' + result + ')');
+			statusMessage(response.Message, 'good');
+			
+			$('#TestSessionObjID').val(response.TestSessionObjID);
+
+/*
+			if($('#right #ModelAdminPanel form').hasClass('validationerror')) {
+				statusMessage(ss.i18n._t('ModelAdmin.VALIDATIONERROR', 'Validation Error'), 'bad');
+			} else {
+				statusMessage(ss.i18n._t('ModelAdmin.SAVED', 'Lots Checked'), 'good');
+			}
+*/
+			// TODO/SAM: It seems a bit of a hack to have to list all the little updaters here. 
+			// Is livequery a solution?
+			Behaviour.apply(); // refreshes ComplexTableField
+		}, 'html');
+		return false;
+	});
+	
+	function statusMessage(msg, type){
+		$('#statusmessage')[0].innerHTML = msg;
+	}
+	
+		
+	})
+})(jQuery);
+
 Behaviour.register({
+
+
+	
+
 	'div.passfail input' : {
 		initialize : function() {
 			this.step = this.parentNode;
@@ -37,7 +77,6 @@ Behaviour.register({
 					Element.removeClassName(this.statusInputs[candStatus].parentNode, 'selected');
 				}
 			}
-			console.log(this);
 			//this.failReason.style.display = (status == 'pass') ? 'none' : '';
 			this.note.style.display = '';
 		}
