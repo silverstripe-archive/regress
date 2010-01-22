9504826ffb8b352a74550667a7cd0955fdf77b49
rspittel <rspittel@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2010-01-22 00:36:54 +0000

    ENHANCEMENT: - renamed teststep to scenario - added some methods for template rendering SessionStepResult, IsOutcomePass, IsOutcomeSkip and IsOutcomeFail - added Markdown support
    git-svn-id: http://svn.silverstripe.com/modules/regress@97418 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- trunk/code/TestStep.php
+++ trunk/code/TestStep.php
@@ -1,13 +1,82 @@
 <?php
+/**
+ * @package regress
+ * @subpackage code
+ */
 
+require_once('../markdown/thirdparty/Markdown/markdown.php');
+
+/**
+ * Scenario
+ */
 class TestStep extends DataObject {
 	static $db = array(
-		"Step" => "Text",
+		"Step" => "MarkdownText",
 		'Sort' => 'Int', 
 	);
+	
 	static $has_one = array(
 		"Parent" => "TestSection",
 	);
+	
+	/**
+	 * Human-readable singular name
+	 * @var string
+	 */
+	public static $singular_name = 'Scenario';
+
+	/**
+	 * Human-readable plural name
+	 * @var string
+	 */
+	public static $plural_name = 'Scenarios';
+	
+	/**
+	 * Returns the test session object of a given ID. The ID is passed in as a
+	 * HTTP parameter.
+	 * 
+	 * @return TestSessionObj|Null Instance of the session object.
+	 */
+	function SessionStepResult() {
+		$obj = null;
+
+		$OtherID = Controller::curr()->urlParams['OtherID'];
+		if($OtherID) {
+			if(is_numeric($OtherID)) {
+				$dataobjectset = DataObject::get("StepResult", "TestStepID = $this->ID AND TestSessionID = $OtherID");
+				if ($dataobjectset != null) {
+					$obj = $dataobjectset->First();
+				}
+			}
+		}
+		return $obj;
+	}
+	
+	function IsOutcomePass() {
+		$obj = $this->SessionStepResult();		
+		if ($obj) return $obj->Outcome == 'pass';
+	}
+
+	function IsOutcomeSkip() {
+		$obj = $this->SessionStepResult();		
+		if ($obj) return $obj->Outcome == 'skip';
+	}
+
+	function IsOutcomeFail() {
+		$obj = $this->SessionStepResult();		
+		if ($obj) return $obj->Outcome == 'fail';
+	}
+	
+	
+	/**
+	 * Return the text stored in Step formatted as HTML, using Markdown for the
+	 * formatting.
+	 *
+	 * @return string
+	 */
+	function StepMarkdown() {
+		return Markdown($this->Step);
+	}
 	
 	function KnownIssues() {
 		if(is_numeric($this->ID)) {
@@ -28,7 +97,7 @@ class TestStep extends DataObject {
 	}
 	
 	function StepNote() {
-		return ( $this->KnownIssues() || $this->PassNote() || $this->SkipNotes() );
+		return ( $this->KnownIssues() || $this->PassNotes() || $this->SkipNotes() );
 	}
 }
 
