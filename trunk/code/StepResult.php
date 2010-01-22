2ee83b0a65c4631056172f5dbe5f4de9b4fedf18
rspittel <rspittel@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2010-01-22 00:39:34 +0000

    MINOR: added documentation.
    git-svn-id: http://svn.silverstripe.com/modules/regress@97421 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- trunk/code/StepResult.php
+++ trunk/code/StepResult.php
@@ -1,43 +1,80 @@
 <?php
+/**
+ * @package regress
+ * @subpackage code
+ */
 
+/**
+ * A StepResult is the instance of a test-result for a scenario (test-step).
+ *
+ * Each step-result is related to one scenario (test-step) and stores the 
+ * information about the test results for the particular scenario. A scenario
+ * can pass it's test, fail or can be skipped by the test person.
+ */ 
 class StepResult extends DataObject {
+	
 	static $db = array(
-		"Outcome" => "Enum('pass,fail,skip,','')",
-		"Note" => "Text",
+		"Outcome"        => "Enum('pass,fail,skip,','')",
+		"Note"           => "Text",
 		"ResolutionDate" => "Datetime",
 	);
+	
 	static $has_one = array(
 		"TestSession" => "TestSessionObj",
-		"TestPlan" => "TestPlan",
-		"TestStep" => "TestStep",
+		"TestPlan"    => "TestPlan",
+		"TestStep"    => "TestStep",
 	);
 	
 	function ResolveActionLink() {
 		return "StepResult_Controller/resolve/$this->ID";
 	}
+	
 	function UnresolveActionLink() {
 		return "StepResult_Controller/unresolve/$this->ID";
 	}
 }
 
+/**
+ * Controller Class for Step Result {@link StepResult}.
+ *
+ */
 class StepResult_Controller extends Controller {
+	
+	/**
+	 * Returns a requested Step-Result
+	 *
+	 * Returns the step-result for a given id. The ID needs to be passed 
+	 * through as an URL-parameter.
+	 *
+	 * @return StepResult|null
+	 */
 	function StepResult() {
 		return DataObject::get_by_id("StepResult", $this->urlParams['ID']);
 	}
-	
+
+	/**
+	 * Marks the step-result as resolved.
+	 *
+	 * Set the flag of the step-result to 'resolved'.
+	 */
 	function resolve() {
 		$sr = $this->StepResult();
 		$sr->ResolutionDate = date('Y-m-d h:i:s');
 		$sr->write();
 		Director::redirectBack();
 	}
+	
+	/**
+	 * Unmarks the step-result as resolved.
+	 *
+	 * Set the flag of the step-result to 'unresolved'.
+	 */
 	function unresolve() {
 		$sr = $this->StepResult();
 		$sr->ResolutionDate = null;
 		$sr->write();
 		Director::redirectBack();
 	}
-	
 }
 
 ?>
