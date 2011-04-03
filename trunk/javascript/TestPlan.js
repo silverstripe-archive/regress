e3ad47755fff69616222e5d1cf64a761713bfb07
rspittel <rspittel@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2011-04-03 23:32:28 +0000

    ENHANCEMENT: JS logic to show/hide severity radio buttons, tooltip changes for severity rating.
    git-svn-id: http://svn.silverstripe.com/modules/regress@117670 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- trunk/javascript/TestPlan.js
+++ trunk/javascript/TestPlan.js
@@ -6,7 +6,6 @@ setInterval(function() {
 }, 180*1000);
 
 function statusMessage(msg, type){
-	
 	if (type == 'good') {
 		$('#statusmessage').removeClass('badMsg').addClass('goodMsg');
 	} else 
@@ -71,103 +70,146 @@ function saveDraft(successMsg) {
 }
 
 (function($) {
-$(document).ready(function() {
+	$(document).ready(function() {
 	
-	$('form[name=session]').changeAwareForm();
+		$('form[name=session]').changeAwareForm();
 	
-	/**
-	 * Auto save 
-	 */ 
-	setInterval(function() {
-		// If one or more outcomes have been set
-		if($(".passfail input[type=radio]:checked").length > 0) {
-			saveDraft('Autosaved');
-		}
-	}, 180*500);
-	
-	/**
-	 * Save draft
-	 */
-	initialised = false;
-	$('input[name=action_doSaveSession]').livequery('click', function(){
-		saveDraft('');
-		return false;
-	});
-
-	/**
-	 * Toggle the status of the perform test form so that tester can modify the
-	 * scenarios during the test.
-	 */
-	$('a.editModeLink').livequery('click', function(){
-		
-		var editModeClass = 'inEditMode';
-		
-		// the link/page is in edit mode, change to readonly mode 
-		if ($(this).hasClass(editModeClass)) {
-			
-			// disable the editing
-			$('.content').editable('disable', null);
-			
-			// remove editable inputs
-			resetEditableInputs();
-			
-			// add classname
-			$('.content').removeClass('changeable').addClass('readonly');
-			
-			jQuery.each ($('.content'),function() {
-				this.title = 'Scenario';
-			});
-			
-			$(this).text("Change to Edit Mode");
-			statusMessage('Page is set to read-only mode.','good');
-			
-			// change the link class
-			$(this).toggleClass(editModeClass);
-			
-		} else { // change to editable mode
-			
-			if (initialised == false) {
-				$('.content').editable('scenario/save', {
-					loadurl   : 'scenario/load',
-					loadtext  : 'Loading data from server...',
-					type      : 'textarea',
-			        indicator : 'Saving...',
-			        tooltip   : 'Click to edit...',
-			        cancel    : 'Cancel',
-			        submit    : 'OK',
-					rows      : 15,
-					width	  : '99%',
-					onblur	  : 'ignore',
-					indicator : "<img src='images/indicator.gif"
-				});
-				
-				$(this).text("Set to read-only mode");
-				initialised =true;
-				
-			} else {
-				// disable the editing
-				$('.content').editable('enable', null);
-				$(this).text("Set to read-only mode");				
+		/**
+		 * Auto save 
+		 */ 
+		setInterval(function() {
+			// If one or more outcomes have been set
+			if($(".passfail input[type=radio]:checked").length > 0) {
+				saveDraft('Autosaved');
 			}
-			
-			// add classname
-			$('.content').removeClass('readonly').addClass('changeable');
-
-			statusMessage('<ul><li>To modify scenarios, click on the green sections of the page.</li><li>For more information about text-formatting and markdown, <br/> please <a href="http://daringfireball.net/projects/markdown/syntax" target="_new">click here</a>.</li></ul>','good');
-			
-			// change the link class
-			$(this).toggleClass(editModeClass);
-		}		
-		return false;
-	});
+		}, 180*500);
 	
-	// remove editable inputs
-	$('.content').click(function() {
-		id = $(this).attr('id');
-		resetEditableInputs(id);
-	});
+		/**
+		 * Save draft
+		 */
+		initialised = false;
+		$('input[name=action_doSaveSession]').livequery('click', function(){
+			saveDraft('');
+			return false;
+		});
+
+		/**
+		 * Toggle the status of the perform test form so that tester can modify the
+		 * scenarios during the test.
+		 */
+		$('a.editModeLink').livequery('click', function(){
+		
+			var editModeClass = 'inEditMode';
+		
+			// the link/page is in edit mode, change to readonly mode 
+			if ($(this).hasClass(editModeClass)) {
+			
+				// disable the editing
+				$('.content').editable('disable', null);
+			
+				// remove editable inputs
+				resetEditableInputs();
+			
+				// add classname
+				$('.content').removeClass('changeable').addClass('readonly');
+			
+				jQuery.each ($('.content'),function() {
+					this.title = 'Scenario';
+				});
+			
+				$(this).text("Change to Edit Mode");
+				statusMessage('Page is set to read-only mode.','good');
+			
+				// change the link class
+				$(this).toggleClass(editModeClass);
+			
+			} else { // change to editable mode
+			
+				if (initialised == false) {
+					$('.content').editable('scenario/save', {
+						loadurl   : 'scenario/load',
+						loadtext  : 'Loading data from server...',
+						type      : 'textarea',
+				        indicator : 'Saving...',
+				        tooltip   : 'Click to edit...',
+				        cancel    : 'Cancel',
+				        submit    : 'OK',
+						rows      : 15,
+						width	  : '99%',
+						onblur	  : 'ignore',
+						indicator : "<img src='images/indicator.gif"
+					});
+				
+					$(this).text("Set to read-only mode");
+					initialised =true;
+				
+				} else {
+					// disable the editing
+					$('.content').editable('enable', null);
+					$(this).text("Set to read-only mode");				
+				}
+			
+				// add classname
+				$('.content').removeClass('readonly').addClass('changeable');
+
+				statusMessage('<ul><li>To modify scenarios, click on the green sections of the page.</li><li>For more information about text-formatting and markdown, <br/> please <a href="http://daringfireball.net/projects/markdown/syntax" target="_new">click here</a>.</li></ul>','good');
+			
+				// change the link class
+				$(this).toggleClass(editModeClass);
+			}		
+			return false;
+		});
+	
+		// remove editable inputs
+		$('.content').click(function() {
+			id = $(this).attr('id');
+			resetEditableInputs(id);
+		});
 
 	});
+
+	function buildTableOfContent() {
+		var html = '';
+		var list = ''; 
+		var location = window.location;
+		var url = location.protocol + '//' + location.host + location.pathname;
+
+		$('.anchor').each( function() { 
+			list += '<li><a href="' + url + '#' + $(this).attr('id') + '">' + $(this).text() + '</a></li>'; 
+		});
+	
+		topLink = location.href.replace(/#/g, '') + "#body";
+
+		// add show table of content link to the leftPanel
+		$('.leftPanel').prepend('<p class="showHideIndex"><a id="showIndex" href="#">Show Index</a></p>')
+	
+		html = '<div id="tableOfContent">';
+		html += '<p class="showHideIndex"><a id="hideIndex" href="#">Hide Index</a></p>'
+		html += '<h3>Index <a id="topLink" href="javascript:scroll(0,0)">(Go to top)</a></h3>';
+		html += '<ul>' + list + '</ul></div>';
+		$('body').append(html);
+	}
+	
+	/** 
+	 * Hide severity radio buttons for passed, skipped and non-selected steps.
+	 */
+	function initiateSeverityVisibility() {
+		$('.passfail input[type=radio][value=fail]:checked').each(function() {
+			var step = $(this).parents('li.scenario');
+			step.children('div.failseverity').children('label').show();
+		});	}
+
+	/**
+	 * Count total number of test-steps for this test-session. Used to 
+	 * populate value into a hidden form field to store the total number
+	 * of tests alongside to the session object.
+	 */
+	function countTestSteps() {
+		var list = 0; 
+		var location = window.location;
+		$('li.scenario').each( function() { list ++; });		
+		$('input[name=NumTestSteps]')[0].value = list;
+	}	
 	
 	/**
 	 * Reset all jeditable form in all .content div, except the one whose id provided
@@ -217,6 +259,8 @@ $(document).ready(function() {
 	
 	countTestSteps();
 	
+	initiateSeverityVisibility();
+	
 	$('a#hideIndex').livequery('click', function() {
 		$('#tableOfContent').hide(); 
 		return false;
@@ -226,29 +270,7 @@ $(document).ready(function() {
 		$('#tableOfContent').show(); 
 		return false;
 	});
-	
-	function buildTableOfContent() {
-		var html = '';
-		var list = ''; 
-		var location = window.location;
-		var url = location.protocol + '//' + location.host + location.pathname;
-
-		$('.anchor').each( function() { 
-			list += '<li><a href="' + url + '#' + $(this).attr('id') + '">' + $(this).text() + '</a></li>'; 
-		});
 		
-		topLink = location.href.replace(/#/g, '') + "#body";
-	
-		// add show table of content link to the leftPanel
-		$('.leftPanel').prepend('<p class="showHideIndex"><a id="showIndex" href="#">Show Index</a></p>')
-		
-		html = '<div id="tableOfContent">';
-		html += '<p class="showHideIndex"><a id="hideIndex" href="#">Hide Index</a></p>'
-		html += '<h3>Index <a id="topLink" href="javascript:scroll(0,0)">(Go to top)</a></h3>';
-		html += '<ul>' + list + '</ul></div>';
-		$('body').append(html);
-	}
-	
 	$('div.failseverity input').livequery('click', function() {
 		var step = $(this).parents('li.scenario');
 		step.addClass('fail').removeClass('pass skip');
@@ -260,17 +282,12 @@ $(document).ready(function() {
 		var step = $(this).parents('li.scenario');
 		step.removeClass('pass fail skip').addClass(this.value);
 		step.children('div.failseverity').children().children('input').attr('checked',false);
+		
+		if (this.value == 'fail') {
+			step.children('div.failseverity').children('label').show();
+		} else {
+			step.children('div.failseverity').children('label').hide();
+		}
 	});	
 	
-	
-	function countTestSteps() {
-		var list = 0; 
-		var location = window.location;
-
-		$('li.scenario').each( function() { 
-			list ++;
-		});
-		
-		$('input[name=NumTestSteps]')[0].value = list;
-	}	
 })(jQuery);
--- trunk/templates/Includes/TestStep.ss
+++ trunk/templates/Includes/TestStep.ss
@@ -10,10 +10,24 @@
 	</div>
 
 	<div class="failseverity">
-		<label class="severity1"><input type="radio" value="Severity1" title="major impact, testing cannot continue" name="Severity[$ID]" <% if IsSeverity1 %>checked="true"<% end_if %> /><strong>Critical</strong></label>
-		<label class="severity2"><input type="radio" value="Severity2" title="major impact, potentially a workaround existis, testing can continue" name="Severity[$ID]" <% if IsSeverity2 %>checked="true"<% end_if %> /><strong>High</strong></label>
-		<label class="severity3"><input type="radio" value="Severity3" title="medium impact, e.g., usability problem" name="Severity[$ID]" <% if IsSeverity3 %>checked="true"<% end_if %> />Medium</label>
-		<label class="severity4"><input type="radio" value="Severity4" title="minor or no impact, e.g., cosmetic error" name="Severity[$ID]" <% if IsSeverity4 %>checked="true"<% end_if %> />Low</label>
+		<span>&nbsp;</span>
+		<label class="severity_header" style="display: none;">Severity Rating:</label>
+		<label class="severity1" title="major impact, testing cannot continue" style="display: none;">
+			<input type="radio" value="Severity1" name="Severity[$ID]" <% if IsSeverity1 %>checked="true"<% end_if %> />
+			<strong>Critical</strong>
+		</label>
+		<label class="severity2" title="major impact, potentially a workaround existis, testing can continue" style="display: none;">
+			<input type="radio" value="Severity2" name="Severity[$ID]" <% if IsSeverity2 %>checked="true"<% end_if %> />
+			<strong>High</strong>
+		</label>
+		<label class="severity3" title="medium impact, e.g., usability problem" style="display: none;">
+			<input type="radio" value="Severity3" name="Severity[$ID]" <% if IsSeverity3 %>checked="true"<% end_if %> />
+			<span>Medium</span>
+		</label>
+		<label class="severity4" title="minor or no impact, e.g., cosmetic error" style="display: none;">
+			<input type="radio" value="Severity4" name="Severity[$ID]" <% if IsSeverity4 %>checked="true"<% end_if %> />
+			<span>Low</span>
+		</label>
 	</div>
 
 	<div id='scenarioContent_$ID' class="content">
